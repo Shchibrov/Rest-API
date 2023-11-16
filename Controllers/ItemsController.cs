1364b5c801da1cc97e0bd56483d2ec17db24d32d
Андрей Щибров <Schibrov@outlook.com>
2023-11-16 20:29:22 +0300

    Удален лишний контроллер

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,38 +1,37 @@
-﻿    using Microsoft.AspNetCore.Mvc;
-    using Rest_API.Models;
-    using System.Collections.Generic;
-    
-    namespace Rest_API.Controllers // Замените на актуальное пространство имен вашего проекта
+﻿using Microsoft.AspNetCore.Mvc;
+using Rest_API.Models;
+
+namespace Rest_API.Controllers // Замените на актуальное пространство имен вашего проекта
 {
-        [ApiController]
-        [Route("[controller]")]
-        public class ItemsController : ControllerBase
-        {
-            private static readonly List<Item> items = new List<Item>
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<Item> items = new List<Item>
         {
             new Item { Id = 1, Name = "Item 1" },
             new Item { Id = 2, Name = "Item 2" },
             // Предварительно заполненные элементы
         };
 
-            // GET: /Items
-            [HttpGet]
-            public IEnumerable<Item> Get()
-            {
-                return items;
-            }
+        // GET: /Items
+        [HttpGet]
+        public IEnumerable<Item> Get()
+        {
+            return items;
+        }
 
-            // GET: /Items/{id}
-            [HttpGet("{id}")]
-            public ActionResult<Item> GetItem(int id)
+        // GET: /Items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(int id)
+        {
+            var item = items.Find(i => i.Id == id);
+            if (item == null)
             {
-                var item = items.Find(i => i.Id == id);
-                if (item == null)
-                {
-                    return NotFound();
-                }
-                return item;
+                return NotFound();
             }
+            return item;
+        }
 
         // POST: /Items
         [HttpPost]
@@ -53,32 +52,32 @@
 
         // PUT: /Items/{id}
         [HttpPut("{id}")]
-            public IActionResult PutItem(int id, Item updatedItem)
+        public IActionResult PutItem(int id, Item updatedItem)
+        {
+            var item = items.Find(i => i.Id == id);
+            if (item == null)
             {
-                var item = items.Find(i => i.Id == id);
-                if (item == null)
-                {
-                    return NotFound();
-                }
-
-                item.Name = updatedItem.Name;
-                // Обновите остальные свойства, если они есть
-
-                return NoContent();
+                return NotFound();
             }
 
-            // DELETE: /Items/{id}
-            [HttpDelete("{id}")]
-            public IActionResult DeleteItem(int id)
-            {
-                var itemIndex = items.FindIndex(i => i.Id == id);
-                if (itemIndex == -1)
-                {
-                    return NotFound();
-                }
+            item.Name = updatedItem.Name;
+            // Обновите остальные свойства, если они есть
 
-                items.RemoveAt(itemIndex);
-                return NoContent();
+            return NoContent();
+        }
+
+        // DELETE: /Items/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteItem(int id)
+        {
+            var itemIndex = items.FindIndex(i => i.Id == id);
+            if (itemIndex == -1)
+            {
+                return NotFound();
             }
+
+            items.RemoveAt(itemIndex);
+            return NoContent();
         }
     }
+}
